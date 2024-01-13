1693f93eebec39e0c60789ba75b228a2ce3fc70c
bolajio <108666295+Bolaji18@users.noreply.github.com>
2024-01-12 23:45:45 -0800

    Update views.py
    user login and authentication modifications

--- user/views.py
+++ user/views.py
@@ -7,6 +7,18 @@ from .forms import NewUserForm
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm
 def index(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+              form = "welcome you have successfully logged in"
+              return render(request=request, template_name="index.html", context={"form":form})
+        else:
+             form = "Invalid Username or Password"
+             return render(request=request, template_name="index.html", context={"form":form})
 
 
     form = AuthenticationForm()
@@ -32,4 +44,3 @@ def create(request):
 
    form= NewUserForm()
    return render(request=request, template_name="create.html", context={"form":form})
-
