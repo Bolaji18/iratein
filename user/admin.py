eb6d7052524598039997e89a57136cc8ff2ff132
bolajio <108666295+Bolaji18@users.noreply.github.com>
2024-01-16 04:55:20 -0800

    Update admin.py
    added models to admin

--- user/admin.py
+++ user/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from .models import message
+from .models import add
+from .models import texts
+from .models import textsrecieved
+
+admin.site.register(message)
+admin.site.register(add)
+admin.site.register(texts)
+admin.site.register(textsrecieved)
