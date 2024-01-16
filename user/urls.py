a202846d1974ad4f0f3922490d66d5dad9672f4f
bolajio <108666295+Bolaji18@users.noreply.github.com>
2024-01-16 04:52:05 -0800

    Update urls.py
    addition changes made

--- user/urls.py
+++ user/urls.py
@@ -3,8 +3,15 @@ from . import views
 from django.contrib.auth.views import LogoutView
 
 urlpatterns =[
+    path('message/', views.message, name='message'),
+
+    path('message/<int:randoms>/<str:sender>', views.shows, name='shows'),
+    path('message/<int:randoms>/<str:receiver>', views.shows2, name='shows2'),
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
+    path('accounts/login/message', views.message, name='messagess'),
+    path('add', views.add, name='add'),
+    path('add2', views.add2, name='add2'),
+    path('logout', LogoutView.as_view(), name='logout'),
 
 ]
-
